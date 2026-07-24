5730b3aa632c18a4d8c8318dd71d4ad8de8f6592
Sonny Brown <lowlifearcade@gmail.com>
2026-07-24 11:21:39 -0700

    add random data

--- src/server.js
+++ src/server.js
@@ -1,7 +1,25 @@
 import http from 'http';
+import { DatabaseSync } from 'node:sqlite';
+import { randomUUIDv7 } from 'node:crypto';
 
-const { PORT } = process.env;
+const { PORT, ENV } = process.env;
+
+const db = new DatabaseSync(':memory:');
+db.exec(`
+    CREATE TABLE IF NOT EXISTS visitor (
+        id TEXT PRIMARY KEY,
+        agent TEXT
+    )
+`);
+
+console.log(randomUUIDv7())
 
 http.createServer(async (req, resp) => {
-    resp.end('Buy Now');
+    const ua = req.headers['user-agent'];
+    const cookie = req.headers['cookie']
+    db.prepare(`
+        INSERT INTO visitor (id, agent)
+        VALUES (?, ?)
+    `).run(randomUUIDv7(), ua)
+    resp.end('Buy Now '+ ua + cookie.split(';').join('\n'));
 }).listen(PORT || 4000, () => console.log('running on ' + PORT));
