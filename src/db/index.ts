56282e01f1bd9a109410c65f5864e2cd29436c8d
Sonny Brown <lowlifearcade@gmail.com>
2026-07-29 16:27:07 -0700

    fix vars for db

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,12 +2,13 @@
 import pg from 'pg';
 
 export const db = new pg.Pool({
-    // connectionString: process.env.DATABASE_URL,
-    host: process.env.POSTGRES_HOST ?? 'localhost',
-    port: Number(process.env.POSTGRES_PORT ?? 5432),
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.RENDER ? { rejectUnauthorized: false } : undefined,
+    // host: process.env.POSTGRES_HOST ?? 'localhost',
+    // port: Number(process.env.POSTGRES_PORT ?? 5432),
+    // user: process.env.POSTGRES_USER,
+    // password: process.env.POSTGRES_PASSWORD,
+    // database: process.env.POSTGRES_DB,
 });
 
 export async function query(string: string, values?: any) {
